21d05f366628acf4d5100f30792e374674fabba4
setarehsoltanieh-glitch <setareh.soltanieh@georgian.io>
2026-03-19 12:11:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# Render Workflows Template - Python
+
+> [!IMPORTANT]
+> **[Render Workflows](https://render.com/docs/workflows) is in public beta.**
+
+Copy this template to your GitHub account by clicking **Use this template > Create a new repository**.
+
+- **To get started with this template,** see [Your First Workflow](https://render.com/docs/workflows-tutorial).
+
+- **To run locally,** see [Local Dev with Render Workflows](https://render.com/docs/workflows-local-development).
+
+When deploying your copy of this template as a workflow service on Render, set the following:
+
+| Option        | Value |
+| ------------- | ----- |
+| Build command | `pip install -r requirements.txt` |
+| Start command | `python main.py` |
---
+++ main.py
@@ -0,0 +1,37 @@
+from render_sdk import Workflows, Retry
+import asyncio
+import random
+
+app = Workflows()
+
+# A minimal task definition
+@app.task
+def calculate_square(a: int) -> int:
+  return a * a
+
+# A task that chains two parallel runs
+# (Must be async to await run results)
+@app.task
+async def sum_squares(a: int, b: int) -> int:
+  result1, result2 = await asyncio.gather(
+    calculate_square(a),
+    calculate_square(b)
+  )
+  return result1 + result2
+
+# A task that flips a coin and retries on "tails"
+# (Illustrates specifying retry logic on failure)
+@app.task(
+  retry=Retry(
+    max_retries=3,
+    wait_duration_ms=1000,
+    backoff_scaling=1.5
+  )
+)
+def flip_coin() -> str:
+  if random.random() < 0.5:
+    raise Exception("Flipped tails! Retrying.")
+  return "Flipped heads!"
+
+if __name__ == "__main__":
+  app.start() # SDK entry point, required for all workflow services
